43a36d78abaa09e1e6bcc21d5c12c9efb2b039cf
Anmol <75719979+hel0wrld@users.noreply.github.com>
2021-12-05 14:43:06 +0530

    Create Hydrothermal Venture_day5-part1.py
    Need numpy

---
+++ Hydrothermal Venture_day5-part1.py
@@ -0,0 +1,31 @@
+import numpy as np
+_input=open("input.txt","r");
+a=_input.readlines();
+m=np.zeros(shape=(1000,1000),dtype=int)
+for line in a:
+	sn=line[:-1].replace(" -> ",",")
+	v=sn.split(',')
+	v=[int(i) for i in v]
+	#print(type(v[0]))
+	print(v)
+	if v[0]==v[2]: 				#x is same so iterate y
+		print("x same")
+		if(v[1]>v[3]):
+			v[1],v[3]=v[3],v[1]
+		for i in range(v[1],v[3]+1):
+			m[v[0]][i]+=1
+			#print("x,y",v[0],i)
+	if v[1]==v[3]: 				#y is same so iterate x
+		print("y same")
+		if(v[0]>v[2]):
+			v[0],v[2]=v[2],v[0]
+		for i in range(v[0],v[2]+1):
+			m[i][v[1]]+=1
+			#print("x,y",i,v[1])
+#print(m.transpose())
+count=0
+for row in m:
+	for x in row:
+		if(x>=2):
+			count+=1
+print(count)
