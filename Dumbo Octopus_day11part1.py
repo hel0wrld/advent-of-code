faf73875d6660ec8d1c875e4b608e35535e9e6c6
Anmol <75719979+hel0wrld@users.noreply.github.com>
2021-12-12 11:26:08 +0530

    Create Dumbo Octopus_day11part1.py
    need to find a nicer way to increment adjacent elements

---
+++ Dumbo Octopus_day11part1.py
@@ -0,0 +1,85 @@
+_input=open("input.txt","r");
+a=_input.readlines();
+a=[line[:-1] for line in a]
+l=[]
+for line in a:
+	x=list(line)
+	x=[int(i) for i in x]
+	l.append(x)
+print(l)
+flash=0
+for day in range(100):
+	for i in range(10):
+		for j in range(10):
+			l[i][j]+=1
+	count=0
+	aa=15
+	while aa>0:
+		aa-=1
+		for i in range(10):
+			for j in range(10):
+				if l[i][j]>9:
+					count+=1
+					l[i][j]=-10
+					if i==0:
+						if j==0:
+							l[1][0]+=1 
+							l[0][1]+=1
+							l[1][1]+=1
+						elif j==9:
+							l[1][j]+=1 
+							l[i][j-1]+=1
+							l[1][j-1]+=1
+						else:
+						 	l[i+1][j]+=1  
+						 	l[i+1][j+1]+=1  
+						 	l[i+1][j-1]+=1  
+						 	l[i][j-1]+=1  
+						 	l[i][j+1]+=1
+					elif i==9:
+						if j==0:
+							l[i-1][j]+=1 
+							l[i][j+1]+=1
+							l[i-1][j+1]+=1
+						elif j==9:
+							l[i-1][j]+=1	
+							l[i][j-1]+=1
+							l[i-1][j-1]+=1					
+						else:
+							l[i-1][j]+=1	
+							l[i-1][j+1]+=1	
+							l[i-1][j-1]+=1	
+							l[i][j-1]+=1	
+							l[i][j+1]+=1
+					elif j==0:
+						l[i-1][j]+=1	
+						l[i+1][j]+=1	
+						l[i][j+1]+=1
+						l[i+1][j+1]+=1
+						l[i-1][j+1]+=1
+					elif j==9:
+						l[i-1][j]+=1	
+						l[i+1][j]+=1	
+						l[i][j-1]+=1
+						l[i+1][j-1]+=1
+						l[i-1][j-1]+=1
+					else:
+						l[i-1][j]+=1
+						l[i-1][j-1]+=1
+						l[i-1][j+1]+=1
+						l[i+1][j]+=1
+						l[i+1][j-1]+=1
+						l[i+1][j+1]+=1
+						l[i][j-1]+=1
+						l[i][j+1]+=1
+		if count==0:
+			break
+	for i in range(10):
+		for j in range(10):
+			if l[i][j]<0:
+				flash+=1
+				l[i][j]=0
+	for line in l:
+		print(line)
+	print()
+print(flash)
