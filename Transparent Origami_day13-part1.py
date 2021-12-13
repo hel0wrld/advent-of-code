d468443b72d73cbd49b70c83dca1697001f27fba
Anmol <75719979+hel0wrld@users.noreply.github.com>
2021-12-13 13:28:24 +0530

    Create Transparent Origami_day13-part1.py
    slow brain moments=inefficient codes. need an input.txt and numpy to work

---
+++ Transparent Origami_day13-part1.py
@@ -0,0 +1,35 @@
+import numpy as np
+_input=open("input.txt","r");
+a=_input.readlines();
+a=[line[:-1] for line in a]
+l=np.full(shape=(1500,1500),fill_value=' ',dtype=str)
+for line in a:
+	x=line.split(',')
+	x=[int(i) for i in x]
+	l[x[0]][x[1]]='#'
+fold=[('x',655),('y',447),('x',327),('y',223),('x',163),('y',111),('x',81),('y',55),('x',40),('y',27),('y',13),('y',6)]
+z=l.transpose()
+for f in fold:
+	if f[0]=='x':
+		for i in range(0,1500):
+			for j in range(f[1],1500):
+				if j==f[1]:
+					z[i][j]=' '
+				elif z[i][j]=='#':
+					z[i][j]=' '
+					dis=j-f[1]
+					z[i][f[1]-dis]='#'
+	if f[0]=='y':
+		for j in range(0,1500):
+			for i in range(f[1],1500):
+				if i==f[1]:
+					z[i][j]=' '
+				elif z[i][j]=='#':
+					z[i][j]=' '
+					dis=i-f[1]
+					z[f[1]-dis][j]='#'
+z=z[:10,:50]
+for x in z:
+	for m in x:
+		print(m,end='')
+	print()
