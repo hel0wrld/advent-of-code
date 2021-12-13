65580c06e0afdec618fc8e63a3d58c3e3992f804
Anmol <75719979+hel0wrld@users.noreply.github.com>
2021-12-13 13:54:57 +0530

    Update Transparent Origami_day13-part2.py
    sliced the indices, i.e "folded" the rows/columns to reduce the number of iterations to get a faster running time

--- Transparent Origami_day13-part2.py
+++ Transparent Origami_day13-part2.py
@@ -2,7 +2,8 @@ import numpy as np
 _input=open("input.txt","r");
 a=_input.readlines();
 a=[line[:-1] for line in a]
-l=np.full(shape=(1500,1500),fill_value=' ',dtype=str)
+row,col=1500,1500
+l=np.full(shape=(row,col),fill_value=' ',dtype=str)
 for line in a:
 	x=line.split(',')
 	x=[int(i) for i in x]
@@ -11,24 +12,26 @@ fold=[('x',655),('y',447),('x',327),('y',223),('x',163),('y',111),('x',81),('y',
 z=l.transpose()
 for f in fold:
 	if f[0]=='x':
-		for i in range(0,1500):
-			for j in range(f[1],1500):
+		for i in range(0,row):
+			for j in range(f[1],col):
 				if j==f[1]:
 					z[i][j]=' '
 				elif z[i][j]=='#':
 					z[i][j]=' '
 					dis=j-f[1]
 					z[i][f[1]-dis]='#'
+		col=f[1]
 	if f[0]=='y':
-		for j in range(0,1500):
-			for i in range(f[1],1500):
+		for j in range(0,col):
+			for i in range(f[1],row):
 				if i==f[1]:
 					z[i][j]=' '
 				elif z[i][j]=='#':
 					z[i][j]=' '
 					dis=i-f[1]
 					z[f[1]-dis][j]='#'
-z=z[:10,:50]
+		row=f[1]
+z=z[:row,:col]
 for x in z:
 	for m in x:
 		print(m,end='')
